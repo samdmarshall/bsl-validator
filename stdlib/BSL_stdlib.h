0c64357b203d99f7c324a438b64df4be7ce0bcb6
samdmarshall <me@samdmarshall.com>
2015-01-16 14:46:05 -0500

    silencing unused args

--- stdlib/BSL_stdlib.h
+++ stdlib/BSL_stdlib.h
@@ -16,18 +16,20 @@
 #if USE_STD_LIB
 #define STDLIB_FUNC_DEC 
 #define STDLIB_FUNC_IMP 
+#define STDLIB_FUNC_ARG
 #else
 #define STDLIB_FUNC_DEC static
 #define STDLIB_FUNC_IMP { return NULL; }
+#define STDLIB_FUNC_ARG __attribute__((unused))
 #endif
 
-STDLIB_FUNC_DEC bsl_variable * stdlib_parse(bsl_context **context, bsl_symbol *symbol, bsl_func_rtype rtype, bsl_func_arg *args, uint32_t arg_count) STDLIB_FUNC_IMP;
+STDLIB_FUNC_DEC bsl_variable * stdlib_parse(STDLIB_FUNC_ARG bsl_context **context, STDLIB_FUNC_ARG bsl_symbol *symbol, STDLIB_FUNC_ARG bsl_func_rtype rtype, STDLIB_FUNC_ARG bsl_func_arg *args, STDLIB_FUNC_ARG uint32_t arg_count) STDLIB_FUNC_IMP;
 
-STDLIB_FUNC_DEC bsl_variable * stdlib_multiply_int(bsl_context **context, bsl_symbol *symbol, bsl_func_rtype rtype, bsl_func_arg *args, uint32_t arg_count) STDLIB_FUNC_IMP;
-STDLIB_FUNC_DEC bsl_variable * stdlib_multiply_float(bsl_context **context, bsl_symbol *symbol, bsl_func_rtype rtype, bsl_func_arg *args, uint32_t arg_count) STDLIB_FUNC_IMP;
+STDLIB_FUNC_DEC bsl_variable * stdlib_multiply_int(STDLIB_FUNC_ARG bsl_context **context, STDLIB_FUNC_ARG bsl_symbol *symbol, STDLIB_FUNC_ARG bsl_func_rtype rtype, STDLIB_FUNC_ARG bsl_func_arg *args, STDLIB_FUNC_ARG uint32_t arg_count) STDLIB_FUNC_IMP;
+STDLIB_FUNC_DEC bsl_variable * stdlib_multiply_float(STDLIB_FUNC_ARG bsl_context **context, STDLIB_FUNC_ARG bsl_symbol *symbol, STDLIB_FUNC_ARG bsl_func_rtype rtype, STDLIB_FUNC_ARG bsl_func_arg *args, STDLIB_FUNC_ARG uint32_t arg_count) STDLIB_FUNC_IMP;
 
-STDLIB_FUNC_DEC bsl_variable * stdlib_divide_int(bsl_context **context, bsl_symbol *symbol, bsl_func_rtype rtype, bsl_func_arg *args, uint32_t arg_count) STDLIB_FUNC_IMP;
-STDLIB_FUNC_DEC bsl_variable * stdlib_divide_float(bsl_context **context, bsl_symbol *symbol, bsl_func_rtype rtype, bsl_func_arg *args, uint32_t arg_count) STDLIB_FUNC_IMP;
+STDLIB_FUNC_DEC bsl_variable * stdlib_divide_int(STDLIB_FUNC_ARG bsl_context **context, STDLIB_FUNC_ARG bsl_symbol *symbol, STDLIB_FUNC_ARG bsl_func_rtype rtype, STDLIB_FUNC_ARG bsl_func_arg *args, STDLIB_FUNC_ARG uint32_t arg_count) STDLIB_FUNC_IMP;
+STDLIB_FUNC_DEC bsl_variable * stdlib_divide_float(STDLIB_FUNC_ARG bsl_context **context, STDLIB_FUNC_ARG bsl_symbol *symbol, STDLIB_FUNC_ARG bsl_func_rtype rtype, STDLIB_FUNC_ARG bsl_func_arg *args, STDLIB_FUNC_ARG uint32_t arg_count) STDLIB_FUNC_IMP;
 
 static bsl_register_func_item STDLIB_FUNCTIONS[] = {
 	{"mul", bsl_db_register_type_int, "(int a, int b)", stdlib_parse, stdlib_multiply_int},
